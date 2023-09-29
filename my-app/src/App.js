5e14e4fc4b9cfc36aada05cc691365df7252ef65
rutik jadhav <112790437+hrutik2@users.noreply.github.com>
2023-09-29 18:41:41 +0530

    Delete my-app/src/App.js

--- my-app/src/App.js
+++
@@ -1,9 +0,0 @@
-import { AllRoutes } from "./Components/AllRoutes"
-import { ProductPage } from "./Components/ProductPage"
-function App() {
-    return(
-  <AllRoutes/>
-    )
-}
-
-export default App
