87722d6ded9cc83efc2b6d5048724931141c313a
rutik jadhav <112790437+hrutik2@users.noreply.github.com>
2023-09-29 18:40:37 +0530

    Delete my-app/src/Components/redux/store.js

--- my-app/src/Components/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, legacy_createStore } from "redux";
-import { combineReducers } from "redux";
-import { ProductReducer } from "./Product/reducer";
-import thunk from "redux-thunk"
-const rootReducer=combineReducers({
-   ProductReducer,
-})
-export const store=legacy_createStore(rootReducer, applyMiddleware(thunk))
