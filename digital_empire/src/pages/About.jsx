15ee02b035efee41e9c9969a2aca224cfdaabb25
RaviTheja1996 <kopparapuraviteja@gmail.com>
2023-10-03 02:19:26 +0530

    made about and footer responsive

--- digital_empire/src/pages/About.jsx
+++ digital_empire/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 
-import { Card, CardHeader, CardBody, CardFooter, SimpleGrid, Heading, Text } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, Heading, Text } from '@chakra-ui/react'
 
 const About = () => {
   return (
